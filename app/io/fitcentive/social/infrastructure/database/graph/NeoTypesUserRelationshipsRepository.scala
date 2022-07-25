630b9feda01d0ff8bde724f4b1213ea502952b1d
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-07-24 20:39:09 -0400

    - Add spatial geo coordinates to public user profile

--- app/io/fitcentive/social/infrastructure/database/graph/NeoTypesUserRelationshipsRepository.scala
+++ app/io/fitcentive/social/infrastructure/database/graph/NeoTypesUserRelationshipsRepository.scala
@@ -1,6 +1,7 @@
 package io.fitcentive.social.infrastructure.database.graph
 
 import io.fitcentive.social.domain.PublicUserProfile
+import io.fitcentive.social.domain.location.Coordinates
 import io.fitcentive.social.domain.types.CustomTypes.GraphDb
 import io.fitcentive.social.infrastructure.contexts.Neo4jExecutionContext
 import io.fitcentive.social.repositories.UserRelationshipsRepository
@@ -8,7 +9,9 @@ import neotypes.implicits.syntax.string._
 import neotypes.generic.auto._
 import neotypes.implicits.syntax.cypher._
 import neotypes.DeferredQueryBuilder
+import org.neo4j.driver.types.Point
 
+import java.time.LocalDate
 import java.util.UUID
 import javax.inject.Inject
 import scala.concurrent.Future
@@ -35,26 +38,53 @@ class NeoTypesUserRelationshipsRepository @Inject() (val db: GraphDb)(implicit v
 
   override def upsertUser(user: PublicUserProfile): Future[PublicUserProfile] =
     CYPHER_UPSERT_USER_INFO(user)
-      .readOnlyQuery[PublicUserProfile]
+      .readOnlyQuery[PublicNeo4jUserProfile]
       .single(db)
+      .map(_.toPublicUserProfile)
 
   override def getUserFollowers(userId: UUID): Future[Seq[PublicUserProfile]] =
     CYPHER_GET_USER_FOLLOWERS(userId)
-      .readOnlyQuery[PublicUserProfile]
+      .readOnlyQuery[PublicNeo4jUserProfile]
       .list(db)
+      .map(_.map(_.toPublicUserProfile))
 
   override def getUserFollowing(userId: UUID): Future[Seq[PublicUserProfile]] =
     CYPHER_GET_USER_FOLLOWING(userId)
-      .readOnlyQuery[PublicUserProfile]
+      .readOnlyQuery[PublicNeo4jUserProfile]
       .list(db)
+      .map(_.map(_.toPublicUserProfile))
 
   override def getUserIfFollowingOtherUser(currentUser: UUID, otherUser: UUID): Future[Option[PublicUserProfile]] =
     CYPHER_GET_USER_IF_FOLLOWING_OTHER_USER(currentUser, otherUser)
-      .readOnlyQuery[Option[PublicUserProfile]]
+      .readOnlyQuery[Option[PublicNeo4jUserProfile]]
       .single(db)
+      .map(_.map(_.toPublicUserProfile))
 }
 
 object NeoTypesUserRelationshipsRepository {
+
+  case class PublicNeo4jUserProfile(
+    userId: UUID,
+    username: Option[String],
+    firstName: Option[String],
+    lastName: Option[String],
+    photoUrl: Option[String],
+    dateOfBirth: Option[LocalDate],
+    locationCenter: Option[Point],
+    locationRadius: Option[Int]
+  ) {
+    def toPublicUserProfile: PublicUserProfile =
+      PublicUserProfile(
+        userId = userId,
+        username = username,
+        firstName = firstName,
+        lastName = lastName,
+        photoUrl = photoUrl,
+        dateOfBirth = dateOfBirth,
+        locationCenter = locationCenter.map(c => Coordinates(latitude = c.y, longitude = c.x)),
+        locationRadius = locationRadius
+      )
+  }
 
   private def CYPHER_UPSERT_USER_INFO(user: PublicUserProfile): DeferredQueryBuilder =
     c"""
@@ -64,7 +94,10 @@ object NeoTypesUserRelationshipsRepository {
         user.firstName = ${user.firstName},
         user.lastName = ${user.lastName},
         user.photoUrl = ${user.photoUrl},
-        user.dateOfBirth = ${user.dateOfBirth}
+        user.dateOfBirth = ${user.dateOfBirth},
+        user.locationRadius = ${user.locationRadius},
+        user.locationCenter =  ${user.locationCenter.map(_.toNeo4jPoint)}
+
       RETURN user"""
 
   private def CYPHER_MAKE_USER_FOLLOW_OTHER(requestingUserId: UUID, targetUserId: UUID): DeferredQueryBuilder =
