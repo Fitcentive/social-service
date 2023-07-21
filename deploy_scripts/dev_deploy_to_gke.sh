c6e739f8d242aaa949efb7caa088d4b9d28fe611
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-07-20 22:38:26 -0400

    feat: Deploy service to fitcentive-dev-03

--- deploy_scripts/dev_deploy_to_gke.sh
+++ deploy_scripts/dev_deploy_to_gke.sh
@@ -3,7 +3,7 @@
 kubectl delete -n social deployment/social-service
 
 # Delete old 1.0 image from gcr
-echo "y" | gcloud container images delete gcr.io/fitcentive-dev-02/social:1.0 --force-delete-tags
+echo "y" | gcloud container images delete gcr.io/fitcentive-dev-03/social:1.0 --force-delete-tags
 
 # Build and push image to gcr
 sbt docker:publish
